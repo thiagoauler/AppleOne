90e93ef015e2ab752ea75eec5b515f299a171c06
Thiago Auler dos Santos <thiagoauler@gmail.com>
2018-05-02 18:37:29 -0300

    Initial Commit

---
+++ AppleOne/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AppleOne
+//
+//  Created by Thiago Auler dos Santos on 02/05/2018.
+//  Copyright © 2018 Thiago Auler dos Santos. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
